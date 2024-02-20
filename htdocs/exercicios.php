9f204fe43733d80bee352ac38f13585727293342
VictorNlc <102592499+VictorNlc@users.noreply.github.com>
2024-02-20 09:34:39 -0300

    Add files via upload

---
+++ htdocs/exercicios.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+    $frequencia_aluno = 87;
+    $frequencia = 120;
+    $frequencia_porcentagem = ($frequencia_aluno / $frequencia) *100;
+
+    printf("a frequencia do aluno foi de %.2f", $frequencia_porcentagem);
+    ?>
+</body>+
+
+</html>
